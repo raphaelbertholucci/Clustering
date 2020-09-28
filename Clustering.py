6662c1ee2d174e398e4a3cb2a8ee3f2ee5f9f8db
Raphael Bertholucci <raphaelbertholucci@gmail.com>
2020-09-28 14:18:00 -0300

    get results without Bankrupt column

--- Clustering.py
+++ Clustering.py
@@ -10,20 +10,17 @@ warnings.filterwarnings('ignore')
 df = pd.read_csv('taiwan_data.csv')
 df.head()
 
-df_Short = df[['Bankrupt']]
+del df['Bankrupt']
 
 import sklearn.cluster as cluster
 
-K=range(1,30)
+K = range(1,30)
 wss = []
 for k in K:
-    kmeans=cluster.KMeans(n_clusters=k,init="random")
-    kmeans=kmeans.fit(df_Short)
+    kmeans=cluster.KMeans(n_clusters=k,init="k-means++")
+    kmeans=kmeans.fit(df)
     wss_iter = round(kmeans.inertia_, 15)
     wss.append(wss_iter)
 
 mycenters = pd.DataFrame({'Clusters' : K, 'WSS' : wss})
 print(mycenters)
-
-sns.scatterplot(x = 'Clusters', y = 'WSS', data = mycenters, marker="+")
-
